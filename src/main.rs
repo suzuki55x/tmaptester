3eb066c6128c9d5a76acbf303f38bbd067d6176f
T. Suzuki <st09d18@icloud.com>
2022-05-09 19:52:50 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn main() -> std::io::Result<()> {
 
             println!("{}", packet_data);
 
-            std::thread::sleep(std::time::Duration::from_millis(50));
+            std::thread::sleep(std::time::Duration::from_millis(500));
         }
 
         std::thread::sleep(std::time::Duration::from_millis(1000));
